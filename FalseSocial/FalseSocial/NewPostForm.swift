976f3365059b0a59a73f120dc7bd098791746bd4
golax <107643818+golax@users.noreply.github.com>
2022-06-21 20:25:00 -0400

    Update NewPostForm.swift
    creates the foundation for the "new post" window, which connects with the create-post button found in the toolbar ('PostsList.swift'). this improved model adds posts to the feed, in addition to creating a new form every time the button is pressed.

--- FalseSocial/FalseSocial/NewPostForm.swift
+++ FalseSocial/FalseSocial/NewPostForm.swift
@@ -9,36 +9,37 @@ import SwiftUI
 
 struct NewPostForm: View {
     
-    @State private var post = Post(title: "", content: "", authorName: "")
+    typealias CreateAction = (Post) -> Void
+    let createAction: CreateAction
+    
+    @State private var post = Post(title: "", content: "", authorName: "") /// initiates title, content, and author's name as local variables, every time a new post is created (@State)
+    @Environment(\.dismiss) private var dismiss /// allows the "form sheet" to disappear/reappear once the submit button is clicked
     
     var body: some View {
         NavigationView {
             Form {
-                
                 Section{
                     TextField("Title", text: $post.title)
                     TextField("Author Name", text: $post.authorName)
                 }
-                
                 Section("Content"){
                     TextEditor(text: $post.content)
                         .multilineTextAlignment(.leading)
                 }
-                
+                Button(action: createPost){ Text("Create Post") }
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.white)
+                    .padding()
+                    .listRowBackground(Color.accentColor)
             }
-            .onSubmit(createPost)
+            .onSubmit(createPost) /// allows the post button to work
             .navigationTitle("New Post")
-            
-            Button(action: createPost){ Text("Create Post") }
-                .font(.headline)
-                .frame(maxWidth: .infinity)
-                .foregroundColor(.white)
-                .padding()
-                .listRowBackground(Color.accentColor)
         }
     }
-    private func createPost() {
-        print("[PostForm] Creating post...")
+    private func createPost() { /// create the post, then dismiss the current view
+        createAction(post)
+        dismiss()
     }
 }
 
@@ -46,6 +47,6 @@ struct NewPostForm: View {
 
 struct NewPostForm_Previews: PreviewProvider {
     static var previews: some View {
-        NewPostForm()
+        NewPostForm(createAction: {_ in })
     }
 }
