5dba26f6d024d978aa165b877acfbc506ac83f7c
golax <107643818+golax@users.noreply.github.com>
2022-06-25 01:33:33 -0400

    Update PostsRepository.swift
    create and fetch posts from Firebase, which acts as the main feed

--- FalseSocial/FalseSocial/PostsRepository.swift
+++ FalseSocial/FalseSocial/PostsRepository.swift
@@ -5,19 +5,27 @@
 //  Created by H on 6/22/22.
 //
 
+/// the connection to Firebase requires a file, which gives the unique access keys
+
 import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
 struct PostsRepository {
-    static let postsReference = Firestore.firestore().collection("posts")
+    static let postsReference = Firestore.firestore().collection("posts") 
     static func create(_ post: Post) async throws {
         let document = postsReference.document(post.id.uuidString)
         try await document.setData(from: post)
     }
+    static func fetchPosts() async throws -> [Post] {
+        let snapshot = try await postsReference
+            .order(by: "timestamp", descending: true)
+            .getDocuments()
+        return snapshot.documents.compactMap{ document in try! document.data(as: Post.self) }
+    }
 }
 
-
+/// reference, create, and fetch posts from Firebase, respectively
 
 /// an async wrapper, as a way to know that the document of posts is saved to Firebase 
 
